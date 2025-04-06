a1e94613d98c552d94bee1f5c4d7a6e60feeabde
Deeksha <109808547+Deekshabhat123@users.noreply.github.com>
2025-04-07 05:22:44 +0530

    Update newsApi.ts

--- src/store/api/newsApi.ts
+++ src/store/api/newsApi.ts
@@ -1,11 +1,13 @@
+// store/api/newsApi.ts
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const newsApi = createApi({
   reducerPath: 'newsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api/' }), // local proxy endpoint
+  baseQuery: fetchBaseQuery({ baseUrl: '/api/' }), // <-- Use local API route
   endpoints: (builder) => ({
     getNews: builder.query({
-      query: (category = 'business') => `news?category=${category}`,
+      query: (category = 'business') => `news?category=${category}`, // --> hits /api/news
     }),
   }),
 });
